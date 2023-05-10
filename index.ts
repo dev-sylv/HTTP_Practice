24c73870fe86f41d619639acc0a0c433c29e4a8b
nicsylvia <nicsylvia15f@gmail.com>
2023-05-10 14:03:36 -0700

    Got the device user is using

--- index.ts
+++ index.ts
@@ -2,13 +2,26 @@ import http, { ServerResponse } from "http";
 
 const server = http.createServer(
   (req: http.IncomingMessage, res: ServerResponse<http.IncomingMessage>) => {
-    res.writeHead();
-    res.write("Hello sylvia, don't be sad");
+    res.writeHead((res.statusCode = 200), {
+      "content-type": "text/html",
+    });
+    console.log("");
+    console.log(req.rawHeaders[7]);
+    const data = req.rawHeaders[7].split('"')[1];
+    const Postdata = req.rawHeaders[4].split("-")[0];
+
+    if (data === undefined) {
+      res.write(`You are using ${Postdata} to access me!`);
+    } else {
+      res.write(`You are using ${data} to access me!`);
+    }
+
     res.end();
   }
 );
 
 server.on("connection", () => {
+  console.log("");
   console.log("Connection established");
 });
 
